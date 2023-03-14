422371a1e50e419b7b9af6da26698b4aef119ed7
vynijales <vynijales@gmail.com>
2023-03-14 14:57:53 -0300

    Creating a model in the Database

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=100) # Máximo de 100 caracteres
+
+    def __str__(self) -> str:
+        return self.nome # Retornando o nome da pessoa, ao invés do objeto
