b82b44bb8a4593558106d2a013f6dc69a094f6c1
vynijales <vynijales@gmail.com>
2023-03-14 16:49:23 -0300

    Django CRUD

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
-from .views import home
+from django.urls import path
+from .views import home, salvar, editar, update, delete
 
 urlpatterns = [
-    path('', home)
+    path('', home),
+    path('salvar', salvar, name='salvar'),
+    path('editar/<int:id>', editar, name='editar'),
+    path('update/<int:id>', update, name='update'),
+    path('delete/<int:id>', delete, name='delete'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Pessoa
 
 # Create your views here.
 
 def home(request):
-    return render(request, "index.html")
+    pessoas = Pessoa.objects.all()
+    return render(request, "index.html", {"pessoas": pessoas})
+
+def salvar(request):
+    vnome = request.POST.get("nome")
+    Pessoa.objects.create(nome=vnome)
+    pessoas = Pessoa.objects.all()
+    return render(request, "index.html", {"pessoas": pessoas})
+
+def editar(request, id):
+    pessoa = Pessoa.objects.get(id=id)
+    return render(request, "update.html", {"pessoa": pessoa})
+
+
+def update(request, id):
+    vnome = request.POST.get("nome")
+    pessoa = Pessoa.objects.get(id=id)
+    pessoa.nome = vnome
+    pessoa.save()
+    return redirect(home)
+
+def delete(request, id):
+    pessoa = Pessoa.objects.get(id=id)
+    pessoa.delete()
+    return redirect(home)
+
