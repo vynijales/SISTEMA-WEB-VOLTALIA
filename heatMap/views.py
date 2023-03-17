3cc2291f0fde6a2ed1d7aaebaa2fc3e5d5ad916b
vynijales <vynijales@gmail.com>
2023-03-17 17:47:15 -0300

    Update heatMap

--- heatMap/views.py
+++ heatMap/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render, redirect
+from .models import Coordenadas, Meses
 
 # Create your views here.
 
 def heatMap(request):
-    return render(request, "heatMap.html")
+    coordenadas = Coordenadas.objects.all()
+    meses = Meses.objects.all()
+
+    return render(request, "heatMap.html", {"coordenadas": coordenadas, "meses": meses})
+
+# def update(request, id):
+#     vmes = request.POST.get("mes")
+#     mes = Meses.objects.get(id=id)
+#     mes = vmes
+#     mes.save()
+#     return redirect(home)
