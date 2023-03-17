c836caa0cd65815c1bd81fd2d3d3a46f441e0fe6
vynijales <vynijales@gmail.com>
2023-03-17 10:55:41 -0300

    Including tables in the admin session

--- heatMap/admin.py
+++ heatMap/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Coordenadas, Meses
+
 
 # Register your models here.
+admin.site.register(Coordenadas)
+admin.site.register(Meses)
--- heatMap/models.py
+++ heatMap/models.py
@@ -1,4 +1,40 @@
 from django.db import models
+
 from .bd.pythonSQL import catchAllbyID
 # Create your models here.
 
+
+class Coordenadas(models.Model):
+    id = models.IntegerField(primary_key=True)
+    latitude = models.CharField(max_length=10)
+    longitude = models.CharField(max_length=10)
+
+    def __str__(self) -> int:
+        return (self.id, [self.latitude, self.longitude]) # Retorna o id e as coordenadas, ao invés do objeto
+
+    class Meta:
+        managed = False
+        db_table = 'coordenadas'
+
+class Meses(models.Model):
+    id = models.OneToOneField(Coordenadas, models.DO_NOTHING, db_column='id', primary_key=True)
+    janeiro21 = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True)
+    fevereiro21 = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True)
+    marco21 = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True)
+    abril21 = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True)
+    maio21 = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True)
+    junho21 = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True)
+    julho21 = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True)
+    agosto21 = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True)
+    setembro21 = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True)
+    outubro21 = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True)
+    novembro21 = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True)
+    dezembro21 = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True)
+    janeiro22 = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True)
+
+    def __str__(self) -> int:
+        return self.id.id # Retornando o id e a intensidade dos meses, ao invés do objeto
+
+    class Meta:
+        managed = False
+        db_table = 'meses'
